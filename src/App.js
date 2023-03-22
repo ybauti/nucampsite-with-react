5d5706042c2f75359336e049bc31e4d200c9e717
Yesenia Bautista <bautiyes@yahoo.com>
2023-03-22 14:12:26 -0700

    Pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { Routes, Route} from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/footer';
@@ -10,7 +13,11 @@ function App() {
     return (
         <div className='App'>
           <Header/>
-          <CampsitesDirectoryPage/>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='contact' element={<ContactPage />} />
+            <Route path='directory' element={<CampsitesDirectoryPage />} />
+          </Routes>
           <Footer/>
         </div>
     );
