14aee22dfd6ae8adac0982204f302ad608a12216
Yesenia Bautista <bautiyes@yahoo.com>
2023-03-22 13:18:40 -0700

    useState

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,6 +4,7 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite =() => {
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+
+export const selectCampsiteById = (id) =>{
+    return CAMPSITES.find((campsite) => campsite.id ===id);
 };
